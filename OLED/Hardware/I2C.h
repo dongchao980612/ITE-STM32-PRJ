ff06447d6f3141f2e06115278308951e25c6c52b
dongchao <dongchao980612@163.com>
2025-12-08 16:54:32 +0800

    fix oled pin

--- OLED/Hardware/I2C.h
+++ OLED/Hardware/I2C.h
@@ -3,12 +3,12 @@
 
 // I2C SCL引脚配置
 #define I2C_SCL_CLK    RCC_APB2Periph_GPIOB  // SCL引脚时钟
-#define I2C_SCL_PIN    GPIO_Pin_7            // SCL引脚号
+#define I2C_SCL_PIN    GPIO_Pin_8            // SCL引脚号
 #define I2C_SCL_PORT   GPIOB                 // SCL引脚端口
 
 // I2C SDA引脚配置
 #define I2C_SDA_CLK    RCC_APB2Periph_GPIOB  // SDA引脚时钟
-#define I2C_SDA_PIN    GPIO_Pin_8            // SDA引脚号
+#define I2C_SDA_PIN    GPIO_Pin_9            // SDA引脚号
 #define I2C_SDA_PORT   GPIOB                 // SDA引脚端口
 
 
